1eb27909bb442ab39e160bbdab0294051f477512
Anton Gushcha <ncrashed@protonmail.com>
2021-07-04 09:21:46 +0300

    Move to rust 2018 mod system

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,3 @@ pub mod connection;
 pub mod storage;
 pub mod sync;
 pub mod utxo;
-
-extern crate bitcoin_hashes;
-extern crate byte_strings;
-extern crate byteorder;
-extern crate chrono;
-extern crate dashmap;
-extern crate futures;
-extern crate rocksdb;
-extern crate tokio;
-extern crate tokio_stream;
-extern crate tokio_util;
-extern crate time;
